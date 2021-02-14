4be93135d302d8c4ec368c10ae322a7bfec605e7
Pootisplank <ialelin3401@gmail.com>
2021-02-13 22:26:50 -0800

    first commit

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,106 @@
 // product-item.js
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+  .price {
+    color: green;
+    font-size: 1.8em;
+    font-weight: bold;
+    margin: 0;
+  }
 
+  .product {
+    align-items: center;
+    background-color: white;
+    border-radius: 5px;
+    display: grid;
+    grid-template-areas: 
+    'image'
+    'title'
+    'price'
+    'add';
+    grid-template-rows: 67% 11% 11% 11%;
+    height: 450px;
+    filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+    margin: 0 30px 30px 0;
+    padding: 10px 20px;
+    width: 200px;
+  }
+
+  .product > button {
+    background-color: rgb(255, 208, 0);
+    border: none;
+    border-radius: 5px;
+    color: black;
+    justify-self: center;
+    max-height: 35px;
+    padding: 8px 20px;
+    transition: 0.1s ease all;
+  }
+
+  .product > button:hover {
+    background-color: rgb(255, 166, 0);
+    cursor: pointer;
+    transition: 0.1s ease all;
+  }
+
+  .product > img {
+    align-self: center;
+    justify-self: center;
+    width: 100%;
+  }
+
+  .title {
+    font-size: 1.1em;
+    margin: 0;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .title:hover {
+    font-size: 1.1em;
+    margin: 0;
+    white-space: wrap;
+    overflow: auto;
+    text-overflow: unset;
+  }
+</style>
+
+<li class="product">
+  <img src="" alt="">
+  <p class="title"></p>
+  <p class="price"></p>
+  <button onclick="alert('Added to Cart!')">Add to Cart</button>
+</li>
+`;
 class ProductItem extends HTMLElement {
-  // TODO
+  static get observedAttributes() {
+    return ["image", "title", "price"];
+  }
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    let shadow = this.shadowRoot;
+    switch (attrName) {
+      case "image":
+        shadow.querySelector("li>img").src = newVal;
+        break;
+      case "title":
+        shadow.querySelector("p.title").innerText = newVal;
+        shadow.querySelector("li>img").alt = newVal;
+        break;
+      case "price":
+        shadow.querySelector("p.price").innerText = newVal;
+        break;
+
+      default:
+    }
+  }
 }
 
-customElements.define('product-item', ProductItem);
+customElements.define("product-item", ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,29 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+window.addEventListener("DOMContentLoaded", async () => {
+  let myStorage = window.localStorage;
+  let productList = document.getElementById("product-list");
+
+  if (myStorage.getItem("data") == null) {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((obj) => JSON.stringify(obj))
+      .then((data) => {
+        myStorage.setItem("data", data);
+      });
+  }
+
+  let itemData = JSON.parse(localStorage.getItem("data"));
+
+  for (index in itemData) {
+    let item = itemData[index];
+    let productItem = document.createElement("product-item");
+
+    productItem.setAttribute("image", item["image"]);
+    console.log(productItem);
+    productItem.setAttribute("title", item["title"]);
+    productItem.setAttribute("price", item["price"]);
+
+    productList.appendChild(productItem);
+  }
 });
